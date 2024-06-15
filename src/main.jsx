7c4671e284971ec16dc6476d2408cbd056fa7d81
vbnj39 <vbnjwieuh@gmail.com>
2024-06-15 01:00:11 -0400

    Update main.jsx hello

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div>
-      <span> 🤌 👌 ♛ 📣 📢 💬 </span> 
-      <p>boss, pay and use</p
-    </div>
+    <App />
   </React.StrictMode>,
 )
